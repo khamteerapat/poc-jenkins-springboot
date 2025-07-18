bd5824bd2b886a5a1d696e286d4dd6794db3974b
khamteerapat <77033711+khamteerapat@users.noreply.github.com>
2025-07-18 11:34:06 +0700

    [starter[ app

---
+++ src/main/java/com/marengyasuo/poc/JenkinpocApplication.java
@@ -0,0 +1,13 @@
+package com.marengyasuo.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinpocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinpocApplication.class, args);
+	}
+
+}
